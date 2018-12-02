fd74f5cc03d91fbfbd5fde77f752afa42395d870
DESKTOP-0NF0H7M\arens <arensonjt@sbcglobal.net>
2018-12-01 22:48:35 -0700

    Second fix for defeated location

--- controller.home.js
+++ controller.home.js
@@ -242,7 +242,7 @@ app.controller('HomeCtrl', ['$scope', '$location', '$interval', 'DataService', '
 	$scope.determineCharY = function(pos){
 		if(pos == "Defeated" || pos == "Not Deployed"){
 			numDefeat +=1;
-			return ((Math.floor((numDefeat-1)/15) * (gridWidth + boxWidth)) + ($scope.rows.length * (gridWidth + boxWidth)) + 32) +"px";
+			return ((Math.floor((numDefeat-1)/30) * (gridWidth + boxWidth)) + ($scope.rows.length * (gridWidth + boxWidth)) + 32) +"px";
 		}
 
 		pos = pos.substring(pos.indexOf(",")+1).trim();
